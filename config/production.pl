ae0b4f283777a53c30741deeec4ac92c7988faf1
p-chin <pchin.taka.1127@gmail.com>
2014-01-02 23:51:45 +0900

    デフォルトでSQLiteに接続してたのでmysql使う様にした

--- config/production.pl
+++ config/production.pl
@@ -1,12 +1,8 @@
-use File::Spec;
-use File::Basename qw(dirname);
-my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
-my $dbpath = File::Spec->catfile($basedir, 'db', 'production.db');
 +{
-    'DBI' => [
-        "dbi:SQLite:dbname=$dbpath", '', '',
-        +{
-            sqlite_unicode => 1,
-        }
-    ],
+   'DBI' => [
+        "dbi:mysql:database=idol",
+        'root',
+         '',
+        {mysql_enable_utf8 => 1}
+   ],
 };
