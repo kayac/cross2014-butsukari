5f0b84494d374a467f853d48bc919633bee5911e
p-chin <pchin.taka.1127@gmail.com>
2014-01-03 02:40:40 +0900

    シリアルコード生成のバッチ書いた(まだ実行中だけど..)

---
+++ tools/generate_serial.pl
@@ -0,0 +1,20 @@
+use 5.016;
+use strict;
+use warnings;
+use FindBin::libs;
+use Getopt::Long;
+
+use Idol;
+
+Idol->bootstrap;
+
+my $count = 0;
+GetOptions( '--count=i' => \$count );
+print "$count回シリアルコードを発行します...";
+
+my $db = Idol->context->db;
+for (0...$count) {
+    $db->serial_register;
+}
+
+print 'done';
