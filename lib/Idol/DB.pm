d2c8975a89c4a12b349e0d4df357e08255f882aa
p-chin <pchin.taka.1127@gmail.com>
2014-01-03 02:39:05 +0900

    DB.pmにシリアルコード生成のメソッド生やしちゃった

--- lib/Idol/DB.pm
+++ lib/Idol/DB.pm
@@ -1,7 +1,38 @@
 package Idol::DB;
+use 5.016;
 use strict;
 use warnings;
 use utf8;
 use parent qw(Teng);
+
+use Sub::Retry;
+use String::Random;
+
+sub serial_register {
+    my $self = shift;
+
+    $self->insert(
+        'serial_code' => {
+            code => $self->_generate_serial_code,
+        }
+    );
+}
+
+sub _generate_serial_code {
+    my $self = shift;
+
+    state $sr = String::Random->new;
+    my $serial_code;
+    $serial_code = retry 10, 0, sub {
+        $sr->randregex('[ABCDEFGHJKLMNPQRSTUVWXYZ23456789]{8}');
+    }, sub {
+        my $code = shift;
+        return $self->single('serial_code', { code => $code }) ? 1 : 0;
+    };
+
+    die 'Generation of serial code failed because nothing' unless $serial_code;
+
+    return $serial_code;
+}
 
 1;
