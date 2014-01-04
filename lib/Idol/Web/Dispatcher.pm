f3e14e828d212b998744ff8e0e2b382b7fce0e2f
p-chin <pchin.taka.1127@gmail.com>
2014-01-04 22:58:00 +0900

    必要そうなルーティングの処理を簡単に書いた

--- lib/Idol/Web/Dispatcher.pm
+++ lib/Idol/Web/Dispatcher.pm
@@ -3,27 +3,68 @@ use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
+use FormValidator::Lite;
 
-any '/' => sub {
-    my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
-    });
-};
-
-post '/reset_counter' => sub {
+get '/' => sub {
     my $c = shift;
-    $c->session->remove('counter');
-    return $c->redirect('/');
+
+    my @music_list = $c->db->search('music');
+
+    $c->render('index.tx', { music_list => \@music_list });
 };
 
-post '/account/logout' => sub {
-    my ($c) = @_;
-    $c->session->expire();
-    return $c->redirect('/');
+get '/vote/{music_id}' => sub {
+    my ($c, $args) = @_;
+
+    my $music_id = $args->{music_id};
+    my $music = $c->db->single('music', { id => $music_id });
+
+    $c->session->set('music' => $music->get_columns);
+    $c->render('vote.tx', { music => $music });
+};
+
+post '/vote/{music_id}' => sub {
+    my ($c, $args) = @_;
+
+    my $music;
+
+    my $validator = FormValidator::Lite->new( $c->req );
+    $validator->set_message(
+        'serial_code.not_null' => 'not null',
+        'serial_code.length' => 'invalid length',
+    );
+    my $res = $validator->check(
+        serial_code => [ 'NOT_NULL', [qw/LENGTH 8/] ],
+    );
+
+    my @err_msgs;
+    if ($validator->has_error) {
+        for my $msg ($validator->get_error_messages) {
+            push @err_msgs, $msg;
+        }
+
+        $music = $c->session->get('music');
+
+        $c->render('vote.tx',
+            {
+                music => $music,
+                err_msgs => \@err_msgs
+            }
+        );
+    }
+    else {
+        $music = $c->session->get('music');
+        $c->render('vote.tx', { music => $music });
+    }
+};
+
+get '/vote/complete' => sub {
+    my $c = shift;
+
+    $c->redirect('/');
+    my $music = $c->session->remove('music');
+
+    $c->render('vote_complete.tx', { music => $music });
 };
 
 1;
