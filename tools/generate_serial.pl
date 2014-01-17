75c492f72c5d57472cfca5261acd57871f1a8e0e
p-chin <pchin.taka.1127@gmail.com>
2014-01-17 16:17:18 +0900

    fix tool message

--- tools/generate_serial.pl
+++ tools/generate_serial.pl
@@ -10,7 +10,7 @@ Idol->bootstrap;
 
 my $count = 0;
 GetOptions( '--count=i' => \$count );
-print "$count回シリアルコードを発行します...";
+print "$count * 1000回シリアルコードを発行します...";
 
 my $db = Idol->context->db;
 for (0...$count) {
